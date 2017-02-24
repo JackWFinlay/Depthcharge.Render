a3cb8465abc70980035f9bbe045cc519bd62a31d
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-02-24 23:40:41 +1300

    Updates

--- app/routes/render.js
+++ app/routes/render.js
@@ -4,5 +4,16 @@ var router 	= express.Router();
 var jsdom   = require("jsdom").jsdom;
 
 router.post('/render', function(req, res) {
-	
+    console.log('html: ' + req.body.htmlBody);
+	let html = req.body.htmlBody;
+    let doc = jsdom(html, {});
+    let window = document.defaultView;
+    var renderedHtml;
+    window.addEventListener('load', function(event){
+        renderedHtml = window.document.documentElement.outerHTML;
+    });
+    res.body.renderedHtml = renderedHtml;
+    res.send();
 });
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 // index.js
 
-var express = require('express');
-var app     = express(); 
-var render  = require('./app/routes/render');
+var express    = require('express');
+var app        = express(); 
+var render     = require('./app/routes/render');
+var bodyParser = require('body-parser');
+
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 var router = express.Router();// get an instance of the express Router
 app.use('/api', render);// prefix routes with '/api/'
+
+
+// Start server
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log('Magic happens on port ' + port); 
