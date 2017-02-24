a1c10c8b78953dbe86b0175b851c61f2c9c831db
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-02-24 22:27:03 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // index.js
+
+var express = require('express');
+var app     = express(); 
+var render  = require('./app/routes/render');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+var router = express.Router();// get an instance of the express Router
+app.use('/api', render);// prefix routes with '/api/'
