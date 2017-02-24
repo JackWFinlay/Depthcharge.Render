327c631f0ef5a4176ce72b173b649b43eb326afb
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-02-25 10:12:34 +1300

    Add support to get Doctype element

--- app/routes/render.js
+++ app/routes/render.js
@@ -1,14 +1,15 @@
 // render.js
-var express = require('express');
-var router 	= express.Router();
-var jsdom   = require("jsdom").jsdom;
+var express           = require('express');
+var router            = express.Router();
+var jsdom             = require("jsdom").jsdom;
+var serializeDocument = require("jsdom").serializeDocument;
 
 router.post('/render', function(req, res) {
 	let html = req.body.htmlBody;
     let doc = jsdom(html, {});
     let window = doc.defaultView;
     window.addEventListener('load', function(event){
-        let renderedHtml = window.document.documentElement.outerHTML;
+        let renderedHtml = serializeDocument(window.document);
         sendResponse(res, renderedHtml);  
     });  
 });
