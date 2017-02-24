0191a3cee40ad224d0a365ae766a0cd82784c1f2
Jack Witbrock Finlay <jack@jackfinlay.com>
2017-02-25 10:07:02 +1300

    Refactor render.js

--- app/routes/render.js
+++ app/routes/render.js
@@ -3,23 +3,18 @@ var express = require('express');
 var router 	= express.Router();
 var jsdom   = require("jsdom").jsdom;
 
-router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now())
-  next()
-})
-
 router.post('/render', function(req, res) {
 	let html = req.body.htmlBody;
     let doc = jsdom(html, {});
     let window = doc.defaultView;
-    var renderedHtml = "";
     window.addEventListener('load', function(event){
-        console.log("loaded");
-        renderedHtml = window.document.documentElement.outerHTML;
-        console.log("renderedHTML: " + renderedHtml);
-        res.send(renderedHtml);
-    });
-    
+        let renderedHtml = window.document.documentElement.outerHTML;
+        sendResponse(res, renderedHtml);  
+    });  
 });
+
+function sendResponse(res, renderedHtml) {
+    res.send(renderedHtml);
+}
 
 module.exports = router;
